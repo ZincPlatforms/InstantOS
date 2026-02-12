ea773d2aa5bf28fadd6409091067f7332bcbc0af
landingpad75 <skydevsmc@outlook.com>
2026-02-12 22:02:18 +0100

    uncovered more framebuffer data to buffer

--- src/graphics/buffer.cpp
+++ src/graphics/buffer.cpp
@@ -6,6 +6,12 @@ Buffer::Buffer(limine_framebuffer* fb) {
     width = fb->width;
     height = fb->height;
     pitch = fb->pitch / 4;
+    red_mask_size = fb->red_mask_size;
+    red_mask_shift = fb->red_mask_shift;
+    green_mask_size = fb->green_mask_size;
+    green_mask_shift = fb->green_mask_shift;
+    blue_mask_size = fb->blue_mask_size;
+    blue_mask_shift = fb->blue_mask_shift;
 }
 
 Buffer::~Buffer() {
