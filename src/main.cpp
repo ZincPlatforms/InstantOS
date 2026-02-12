4affe750b97d71e1c0024524f5baa5baf0f2d834
landingpad75 <skydevsmc@outlook.com>
2026-02-12 22:06:56 +0100

    autopong + set it as initial app

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,26 @@
 extern GDT* gdt;
 
 int main(){
-    const char* argv[] = { "/shell.elf", nullptr };
-    Process* userProc = ProcessExecutor::loadUserBinaryWithArgs("/shell.elf", 1, argv);
-    
+    const char* argv[] = { "/autopong", nullptr };
+    Process* userProc = ProcessExecutor::loadUserBinaryWithArgs("/autopong", 1, argv);
+
+    if (!userProc) {
+        userProc = ProcessExecutor::loadUserBinaryWithArgs("/autopong", 1, argv);
+    }
+
+    if (!userProc) {
+        for(;;) asm volatile("hlt");
+    }
+
     Scheduler::get().addProcess(userProc);
     Scheduler::get().setCurrentProcess(userProc);
     userProc->setState(ProcessState::Running);
-        
+
     gdt->setKernelStack(userProc->getKernelStack());
     Syscall::get().setKernelStack(userProc->getKernelStack());
-        
+
     asm volatile("sti");
-        
+
     switchContext(nullptr, userProc->getContext());
 
     for(;;);
